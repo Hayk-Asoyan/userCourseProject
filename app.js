849fb3ef021a354629f2c690e06c50aa7b518b4c
Hayk-Asoyan <haykasoyan90@gmail.com>
2020-06-13 23:36:55 +0400

    full

---
+++ app.js
@@ -0,0 +1,20 @@
+import { getUsers } from "./api/api.js";
+import { createUserInfoElem } from "./utils/helpers.js";
+const rootElem = document.getElementById("root");
+
+
+async function users() {
+  try {
+    const data = await getUsers(10);
+    data.results.forEach(function (userData) {
+      createUserInfoElem(userData, rootElem);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+users();
+
+
+
